984258e24e985cbc85068dfda26fa0b70cbbcc40
watssupp <jorgesolisruiz@gmal.com>
2022-08-06 16:48:42 -0500

    made JS local storage to add and remove Img

---
+++ my-list.js
@@ -0,0 +1,45 @@
+var form = document.getElementById('form')
+
+var parentDiv = document.getElementById('result')
+
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    var reader = new FileReader()
+
+    var name = document.getElementById("image").files[0].name
+    
+    
+    reader.addEventListener('load', function () {
+        if(this.result && localStorage) {
+            window.localStorage.setItem(name, this.result)          
+            parentDiv.innerHTML = ''
+            showImages()
+        }
+       
+    })
+
+    reader.readAsDataURL(document.getElementById('image').files[0])
+
+    console.log(name)
+
+})
+
+function showImages() {
+    for(let i = 0;i< window.localStorage.length; i++){
+        let res = window.localStorage.getItem(window.localStorage.key(i))
+        var image = new Image()
+        image.src = res
+
+        parentDiv.appendChild(image)
+
+    }
+}
+
+function remove() {
+    window.localStorage.clear()
+    parentDiv.innerHTML = ''
+}
+
+showImages()
