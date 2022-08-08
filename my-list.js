1e33696d6b5dd9a43c20f7788c6892a5c3780042
watssupp <jorgesolisruiz@gmal.com>
2022-08-08 18:59:00 -0500

    better functionality to JS

--- my-list.js
+++ my-list.js
@@ -1,30 +1,40 @@
 var form = document.getElementById('form')
+var form2  = document.getElementById('form2')
+var form3 = document.getElementById('form3')
 
-var parentDiv = document.getElementById('result')
 
 
-form.addEventListener('submit', function (event) {
+var submit = function (event) {
     event.preventDefault()
 
     var reader = new FileReader()
 
-    var name = document.getElementById("image").files[0].name
+    var name = event.target.children[0].children[0].files[0].name
     
     
     reader.addEventListener('load', function () {
         if(this.result && localStorage) {
-            window.localStorage.setItem(name, this.result)          
+            window.localStorage.setItem(name, this.result)
             parentDiv.innerHTML = ''
             showImages()
         }
        
     })
 
-    reader.readAsDataURL(document.getElementById('image').files[0])
+    reader.readAsDataURL(event.target.children[0].children[0].files[0])
 
     console.log(name)
 
-})
+}
+
+var parentDiv = document.getElementById('result')
+var parentDiv2 = document.getElementById('result2')
+var parentDiv3 = document.getElementById('result3')
+
+
+form.addEventListener('submit',submit )
+form2.addEventListener('submit',submit )
+form3.addEventListener('submit',submit )
 
 function showImages() {
     for(let i = 0;i< window.localStorage.length; i++){
