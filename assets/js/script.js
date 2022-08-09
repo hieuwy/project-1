9ce2f780d34324e2b3147cc1f93a90c9651dea87
hieuwy <107073449+hieuwy@users.noreply.github.com>
2022-08-08 19:42:28 -0500

    Added Randomizer

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,34 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
+  var url = 'https://graphql.anilist.co',
+  options = {
+      method: 'POST',
+      headers: {
+          'Authorization': 'Bearer ' + accessToken,
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+      },
+      body: JSON.stringify({
+          query: query
+      })
+  };
+fetch(url, options).then(handleResponse, handleError);
+function handleResponse (response) {
+  console.log(response);
+}
+const base_url = "'https://graphql.anilist.co";
+function searchAnime(event){
+event.preventDefault();
+const form = new FormData(this);
+const query = form.get("search");
+console.log(query);
+fetch('${base_url}/=${query}')
+.then(res=>res.json())
+.then(data=>console.log(data))
+.catch(err=>console.warn(err.message));
+}
+function pageLoaded(){
+const form = document.getElementById('search_form');
+form.addEventListener("submit", searchAnime);
+}
