1acf3400afc5d6555f6762c40b0b25cd5c40dc52
watssupp <jorgesolisruiz@gmal.com>
2022-08-09 14:30:23 -0500

    Added a Shuffel button to the randomizer-card page with JS

--- assets/js/randomizer.js
+++ assets/js/randomizer.js
@@ -4,3 +4,26 @@ var swiper = new Swiper(".mySwiper", {
     grabCursor: true,
   });
 
+  image_array = [
+    "berserk.jpg",
+    "junji.jpg",
+    "parasyte.jpg",
+    "tokyo.jpg",
+    "neverland.jpg",
+    "hs.jpg",
+    "hellsing.jpg",
+    "gantz.jpg"
+    
+    ]
+    
+    function get_random_image(){
+    // Get a random index
+    random_index = Math.floor(Math.random() * image_array.length);
+    
+    // Get an image at the random_index
+    selected_image = image_array[random_index]
+    
+    // Display the image
+    document.getElementById('image_shower').src = `./assets/img/${selected_image}`
+    }
+    
